160a8cee85f212b98d0bd0e93fa6b71d7d3e7d50
nunosousa <edgarpedra99@gmail.com>
2025-11-16 12:41:04 +0000

    Add Kafka topic configuration

---
+++ src/main/java/com/nunostudios/ordermanagement/config/KafkaTopicConfig.java
@@ -0,0 +1,14 @@
+package com.nunostudios.ordermanagement.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic orderStatusUpdatedTopic() {
+        return new NewTopic("order-status-updated", 1, (short) 1);
+    }
+}
