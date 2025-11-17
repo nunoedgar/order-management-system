8a02ffa4eded9c2a6551125ba56afacd41800b5e
nunosousa <nuno.carvalho.pedra.de.sousa@rabobank.nl>
2025-11-17 16:39:19 +0000

    Rename application and test classes from OrderManagementSystem to OrderManagement

--- order-management/src/main/java/com/nunostudios/ordermanagement/OrderManagementApplication.java
+++ order-management/src/main/java/com/nunostudios/ordermanagement/OrderManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderManagementSystemApplication {
+public class OrderManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderManagementSystemApplication.class, args);
+		SpringApplication.run(OrderManagementApplication.class, args);
 	}
 
 }
